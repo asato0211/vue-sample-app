03339d2049234ce653dd5d18ae6432bb6b550d0f
asato0211 <mai0926yhm@gmail.com>
2021-12-26 01:07:46 +0900

    Finish L9

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 1,
-      product: 'Socks',
-      image: './assets/images/socks_blue.jpg',
-      inventory: 11,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-      ]
-    }
-  },
-
-  methods: {
-    addToCart() {
-      this.cart += 1
-    },
-    updatedImage(variantImage) {
-      this.image = variantImage
+      cart: 0,
+      premium: true
     }
   }
 })
-
